67fa13f0c577957fb7d76f5ae69d0e97c9407c37
Olaf Radicke <briefkasten@olaf-radicke.de>
2016-08-24 20:41:55 +0200

    begin with sqlite

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const crypto = require('crypto');
 const randumpath = crypto.randomBytes(30);
 var recursive = require('recursive-readdir');
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+
 console.log(`Enter: http://localhost:3000/${randumpath.toString('hex')}`);
 console.log("AGUANO_CONTENT: " + process.env.AGUANO_CONTENT);
 
@@ -15,6 +18,20 @@ recursive(process.env.AGUANO_CONTENT, function (err, files) {
 	if (err)
   		throw ("Reading directory is faild with:" + err);
 
+	db.serialize(function() {
+		db.run("CREATE TABLE keyword (path TEXT, key TEXT)");
+		var stmt = db.prepare("INSERT INTO keyword VALUES (?,?)");
+		files.forEach(function(path) {
+			path.split(path.sep).forEach(function(key){
+				stmt.run(path, key);
+			})
+		})
+		stmt.finalize();
+
+		db.each("SELECT path, key FROM keyword", function(err, row) {
+		 	console.log(row.path + ": " + row.key);
+		});
+	});
 
 	app.get('/' + randumpath.toString('hex'), function (req, res) {
 		res.send('Hello World!');
